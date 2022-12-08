94e61138e6b9e8457e6bcc7e8ee871aa12acf4d0
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2022-12-08 10:17:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 // JavaScript callback Function
-const names = ['stats','sev',nav]
-const myForEach = (arr, cb) {
-    for (let i = 0; i < arr.length; i++) {
-        const element = arr[i]
-        cb(element)
-    }
-}
-
-myForEach(names, (names)) {
-    console.log(names)
-}
+const divide = (num1, num2) => 2000 / 100;
+divide(2000, 100);
+const square = x => x ** x
+const add = (num2, num3) => num2 + num3;
